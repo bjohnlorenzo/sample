d67463aba24ea2293dbd4b69eb7aa7c34643812d
John Bonjibod <john.bonjibod@merkle.com>
2024-07-10 22:39:22 +0800

    2nd commit

--- src/main/java/com/sample/qrgenerator/QRGenerator.java
+++ src/main/java/com/sample/qrgenerator/QRGenerator.java
@@ -1,4 +1,13 @@
 package com.sample.qrgenerator;
 
 public class QRGenerator {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(final String name) {
+        this.name = name;
+    }
 }
